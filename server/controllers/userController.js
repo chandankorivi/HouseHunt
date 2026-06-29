17bfe058fe4ab4e871395521bc9ca74fb0a60633
chandankorivi <chandankorivi792@gmail.com>
2026-06-29 21:02:36 +0530

    Added Jwt and ensured no duplicate email

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,27 @@
 const User = require("../models/User");
 
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
 const registerUser = async (req,res)=>{
 
     const {name,email,password,role} = req.body;
 
+    const existingUser = await User.findOne({ email });
+
+if (existingUser) {
+    return res.status(400).json({
+        message: "Email already exists"
+    });
+}
+
+const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
         name,
         email,
-        password,
+        password:hashedPassword,
         role
     });
 
@@ -18,6 +32,38 @@ const registerUser = async (req,res)=>{
 
 };
 
+const loginUser = async (req,res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if(!user) {
+        return res.status(400).json({
+            message : "Invalid Email or password"
+        });
+    }
+
+const isMatch = await bcrypt.compare(password,user.password);
+     if(!isMatch){
+        return res.status(400).json({
+            message : "Invalid password"
+        });
+     }
+
+     const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET,
+    {
+        expiresIn:"7d"
+    }
+);
+
+res.json({
+    message:"Login successful",
+    token
+});
+
+};
+
 module.exports = {
-    registerUser
+    registerUser,loginUser
 };
--- server/models/User.js
+++ server/models/User.js
@@ -23,4 +23,6 @@ const userSchema = new mongoose.Schema({
 
 });
 
+
+
 module.exports = mongoose.model("User", userSchema);
